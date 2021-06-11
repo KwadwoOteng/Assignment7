880dbc6703b8778be83597d170776816e6e6c812
KwadwoOteng <79852511+KwadwoOteng@users.noreply.github.com>
2021-06-10 23:50:04 -0700

    first commit

---
+++ test/post.test.js
@@ -0,0 +1,18 @@
+import server from "../server"
+import chai from "chai";
+import chaiHttp from "chail-http"
+
+//styling method
+chai.should();
+
+// middleware
+chai.use(chaiHttp);
+import server from "../server";
+import chai from "chai";
+import chaiHttp from "chail-http";
+
+//styling method
+chai.should();
+
+// middleware
+chai.use(chaiHttp);
